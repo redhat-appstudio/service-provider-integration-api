646e2ee315819be03cc53419ca5befa0a8bead86
Max Shaposhnik <mshaposh@redhat.com>
2021-09-29 18:21:35 +0300

    Add vault backend storage-2

---
+++ src/main/java/org/redhat/appstudio/serviceprovider/service/storage/VaultAccessTokenService.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2021 Red Hat, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.redhat.appstudio.serviceprovider.service.storage;
+
+import io.quarkus.vault.VaultKVSecretEngine;
+import io.quarkus.vault.runtime.client.VaultClientException;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.jboss.logging.Logger;
+
+public class VaultAccessTokenService implements AccessTokenService {
+
+  private static final Logger LOG = Logger.getLogger(VaultAccessTokenService.class);
+
+  private final VaultKVSecretEngine kvSecretEngine;
+
+  private final String vaultPath = "spi/accesstokens";
+
+  public VaultAccessTokenService(VaultKVSecretEngine kvSecretEngine) {
+    this.kvSecretEngine = kvSecretEngine;
+  }
+
+  public Set<AccessToken> fetchAll() {
+    LOG.info("fetchAll");
+    return kvSecretEngine.listSecrets(vaultPath).stream()
+        .map(s -> AccessToken.fromKVMap(kvSecretEngine.readSecret(vaultPath + "/" + s)))
+        .collect(Collectors.toSet());
+  }
+
+  public Optional<AccessToken> get(String name) {
+    LOG.info(name);
+    try {
+      return Optional.of(AccessToken.fromKVMap(kvSecretEngine.readSecret(vaultPath + "/" + name)));
+    } catch (VaultClientException e) {
+      return Optional.empty();
+    }
+  }
+
+  public AccessToken create(AccessToken accessToken) {
+    LOG.info(accessToken);
+    kvSecretEngine.writeSecret(vaultPath + "/" + accessToken.getName(), accessToken.asKVMap());
+    return accessToken;
+  }
+
+  public void update(String name, AccessToken accessToken) {
+    LOG.info(accessToken);
+    kvSecretEngine.writeSecret(vaultPath + "/" + name, accessToken.asKVMap());
+  }
+
+  public void delete(String name) {
+    LOG.info(name);
+    kvSecretEngine.deleteSecret(vaultPath + "/" + name);
+  }
+}
